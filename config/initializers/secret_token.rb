f22f03228efe61b8890e3ed25679886ab21d8062
Jay <jayottovegio@gmail.com>
2012-11-27 20:53:36 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WineTracker::Application.config.secret_token = 'c7d3105babb2ae6de3f38f10a23cacb7a30e1f9b0e727dcbc643cffba9fd8d55b0ef9513128bbcc2bb52a6411445e9d07bfc44c2b9c882ce5b93799080c7d3cc'

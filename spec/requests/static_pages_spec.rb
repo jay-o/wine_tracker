e6cc80db63b92c0f6ed84238ac7675b2c348743a
Jay <jayottovegio@gmail.com>
2012-11-27 22:50:33 -0800

    removed default index

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,17 +6,17 @@ describe "Static pages" do
 
 	describe "Home page" do
 		it "should have the h1 'Sample App'" do
-			visit '/static_pages/home'
+			visit root_path
 			page.should have_selector('h1', :text => 'Sample App')
 		end
 
 		it "should have the base title" do
-			visit '/static_pages/home'
+			visit root_path
 			page.should have_selector('title', :text => "Wine Tracker Beta")
 		end
 
 		it "should not have a custom page title" do
-			visit '/static_pages/home'
+			visit root_path
 			page.should_not have_selector('title', :text => '| Home')
 		end
 	end
